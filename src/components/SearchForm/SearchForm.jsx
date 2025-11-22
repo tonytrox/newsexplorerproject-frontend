4a1341350e7d4714f056e2117205adf68c0b2ecc
Gilbert Carrasco <tonytrox@gmail.com>
2025-11-22 01:12:09 -0500

    SearchForm UI completed

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,43 @@
-import "./searchForm.css";
+import './searchForm.css';
+// import { useContext } from 'react';
+// import { keywordContext } from '../../contexts/keyWordContext';
 
-function SearchForm() {
-    return <div>Search</div>;
+function SearchForm({ handleSearch }) {
+  // const { keyword, setkeyword } = useContext(keywordContext);
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   handleSearch(keyword);
+  // };
+
+  // const handleKeyWord = (event) => {
+  //   setkeyword(event.target.value);
+  // };
+
+  return (
+    <section className="search">
+      <h1 className="search__heading">¿Qué está pasando en el mundo?</h1>
+      <p className="search__subheading">
+        Encuentra las últimas noticias sobre cualquier tema y guárdalas en tu cuenta personal.
+      </p>
+      <form className="search__form">
+        <div className="search__form-container">
+          <input
+            className="search__input"
+            type="text"
+            id="search"
+            // value={keyword}
+            placeholder="Introduce un tema"
+            // onChange={}
+            required
+          />
+          <button type="submit" className="search__button">
+            Buscar
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm;
