19714f92f1f12e1474b57a435bf0af21aa3e64c7
Gilbert Carrasco <tonytrox@gmail.com>
2025-11-26 13:06:00 -0500

    Header UI completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
-import "./header.css";
-import Navigation from "../Navigation/Navigation";
+import './header.css';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
-    return (
-        <div className="header">
-            <Navigation />
-        </div>
-    );
+  return (
+    <div className="header">
+      <h1 className="header__title">NewsExplorer</h1>
+      {/* <Navigation /> */}
+    </div>
+  );
 }
 
 export default Header;
