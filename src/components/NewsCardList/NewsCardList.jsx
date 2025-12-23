86faee99f55725d6bbc699840f4cec8a09108258
Gilbert Carrasco <tonytrox@gmail.com>
2025-12-23 16:30:13 -0500

    create NewsCardList layout component

---
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -0,0 +1,15 @@
+import './newsCardList.css';
+
+function NewsCardList() {
+  return (
+    <section className="news">
+      <div className="news__container">
+        <h2 className="news__title">Resultados de la búsqueda</h2>
+        <ul>tarjeta</ul>
+        <button className="news__button">Ver más</button>
+      </div>
+    </section>
+  );
+}
+
+export default NewsCardList;
--- src/components/SavedNews/SaveNews.jsx
+++ src/components/SavedNews/SaveNews.jsx
@@ -1,12 +1,14 @@
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import SavedNewsHeader from '../SavedNewsHeader/SavedNewsHeader';
+import NewsCardList from '../NewsCardList/NewsCardList';
 
 function SavedNews() {
   return (
     <main className="saved-news">
       {/* <Header /> */}
       <SavedNewsHeader />
+      <NewsCardList />
       <Footer />
     </main>
   );
