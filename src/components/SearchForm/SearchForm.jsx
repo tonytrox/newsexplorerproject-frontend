01f22785f2da63584764200c4853cc7c3a8acfa0
Gilbert Carrasco <tonytrox@gmail.com>
2025-12-29 02:23:34 -0500

    Layout Home, updated styles with responsive design

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,41 +1,28 @@
 import './searchForm.css';
-// import { useContext } from 'react';
-// import { keywordContext } from '../../contexts/keyWordContext';
 
-function SearchForm({ handleSearch }) {
-  // const { keyword, setkeyword } = useContext(keywordContext);
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   handleSearch(keyword);
-  // };
-
-  // const handleKeyWord = (event) => {
-  //   setkeyword(event.target.value);
-  // };
-
+function SearchForm() {
   return (
     <section className="search">
-      <h1 className="search__heading">¿Qué está pasando en el mundo?</h1>
-      <p className="search__subheading">
-        Encuentra las últimas noticias sobre cualquier tema y guárdalas en tu cuenta personal.
-      </p>
-      <form className="search__form">
-        <div className="search__form-container">
-          <input
-            className="search__input"
-            type="text"
-            id="search"
-            // value={keyword}
-            placeholder="Introduce un tema"
-            // onChange={}
-            required
-          />
-          <button type="submit" className="search__button">
-            Buscar
-          </button>
-        </div>
-      </form>
+      <div className="search__container">
+        <h1 className="search__heading">¿Qué está pasando en el mundo?</h1>
+        <p className="search__subheading">
+          Encuentra las últimas noticias sobre cualquier tema y guárdalas en tu cuenta personal.
+        </p>
+        <form className="search__form">
+          <div className="search__form-container">
+            <input
+              className="search__input"
+              type="text"
+              id="search"
+              placeholder="Introduce un tema"
+              required
+            />
+            <button type="submit" className="search__button">
+              Buscar
+            </button>
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
