e2c61685d5fbbaa749f0b83bd4a3cbe2c6820418
Gilbert Carrasco <tonytrox@gmail.com>
2026-01-20 23:36:54 -0500

    Updated SearchForm component input. API key added

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,6 +1,28 @@
+import { useState } from 'react';
 import './searchForm.css';
 
 function SearchForm() {
+  // buscar palabra clave
+  const [keyword, setKeyword] = useState('');
+  const [searchError, setSearchError] = useState('');
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+
+    if (!keyword.trim()) {
+      setSearchError('Por favor, introduzca una palabra clave');
+      console.log(`estado inicial de searchError: ${searchError} `);
+      return;
+    }
+    // Si el campo está vacío (sin texto útil), el `if` se ejecuta. De lo contrario, se evita la validación.
+    // `!keyword.trim()` devuelve `true` solo cuando no hay texto después de quitar espacios.
+  };
+
+  const handleInputChange = (event) => {
+    setKeyword(event.target.value);
+    setSearchError('');
+  };
+
   return (
     <section className="search">
       <div className="search__container">
@@ -8,19 +30,21 @@ function SearchForm() {
         <p className="search__subheading">
           Encuentra las últimas noticias sobre cualquier tema y guárdalas en tu cuenta personal.
         </p>
-        <form className="search__form">
+        <form className="search__form" onSubmit={handleSubmitForm}>
           <div className="search__form-container">
             <input
               className="search__input"
               type="text"
-              id="search"
               placeholder="Introduce un tema"
-              required
+              value={keyword}
+              onChange={handleInputChange}
+              // required
             />
             <button type="submit" className="search__button">
               Buscar
             </button>
           </div>
+          {searchError && <span className="search__error">{searchError}</span>}
         </form>
       </div>
     </section>
---
+++ src/utils/NewsAPI.js
@@ -0,0 +1,2 @@
+const NEWSAPI_KEY = '6c882482d8b64cbf9694358b6b70dc18';
+const BASE_URL = 'https://newsapi.org/v2'; // localhost
